518932fc072d4545a65be5419c823bbe52ae9be6
chanceDira <chancedesire60@gmail.com>
2021-11-13 12:29:27 +0200

    Creating token deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,24 @@
 const TokenFarm = artifacts.require("TokenFarm");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock DAI Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.Deployed()
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.Deployed()  
+
+    // Deploy TokenFarm
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+    const TokenFarm = await TokenFarm.Deployed()
+
+    // Transfer all tokens to TokenFarm (1 million)
+    await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+
+
+
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,16 @@
 pragma solidity 0.5.16;
+import './DappToken.sol';
+import './DaiToken.sol';
+
 
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) {   // _daiToken // means address
+        dappToken = _dappToken;
+        daiToken = _daiToken
+    }
+
 }

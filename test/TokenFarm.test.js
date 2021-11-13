e5de6bb564311acdac471d3dc4d3425d2922a8b2
chanceDira <chancedesire60@gmail.com>
2021-11-13 14:55:39 +0200

    before functionality

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -8,12 +8,28 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
+function tokens(n) {
+    return web3.utils.toWei(n, 'ether');
+}
+
 contract('TokenFarm', (accounts) => {
-    describe('Mock Dai deployment', async() => {
+    let daiToken, dappToken, tokenFarm
+    before(async() => {
+        // Load contracts
+        daiToken = await DaiToken.new()
+        dappToken =await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        // Transfer all Dapp tokens to farm (1 million)
+        // await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+        await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+    })
+
+    describe('Mock DAI deployment', async() => {
         it('has a name', async() => {
-            let daiToken = await DaiToken.new()
             const name = await daiToken.name()
-            assert(name, 'Mock Dai Token')
+            assert(name, 'Mock DAI Token')
         })
     })
 })

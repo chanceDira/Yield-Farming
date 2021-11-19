f41dbd65b082086e5ec75aae9207f2da9594b654
chanceDira <chancedesire60@gmail.com>
2021-11-19 17:49:19 +0200

    Testing invalid Network

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import DappToken from '../abis/DappToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
 import Navbar from './Navbar'
 import './App.css'
 
@@ -10,7 +13,7 @@ class App extends Component {
     await this.loadBlockchainData()
   }
 
-  async loadBlockchainData() {
+  async loadBlockchainData() {  //Loading Data from the Blockchain - Smart contracts deployed on Ganache network
     const web3 = window.web3
 
     const accounts = await web3.eth.getAccounts()
@@ -18,6 +21,33 @@ class App extends Component {
 
     const networkId = await web3.eth.net.getId()
     console.log(networkId)  //5777 networkId for Ganache
+
+    //Load DaiToken
+    const daiTokenData = DaiToken.networks[networkId]
+    if(daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)  //abi - JSON
+      this.setState({ daiToken })
+      // console.log("DaiToken ", daiToken)
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString( )})
+      // console.log({ balance: daiTokenBalance})  // 100 tokens in wei
+    } else {
+      window.alert('DaiToken contract not deployed to detected network. ')
+    }
+
+    //Load DappToken
+    const dappTokenData = DappToken.networks[networkId]
+    if(dappTokenData) {
+      const dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address)  //abi - JSON
+      this.setState({ dappToken })
+      // console.log("DappToken ", dappToken)
+      let dappTokenBalance = await dappToken.methods.balanceOf(this.state.account).call()
+      this.setState({ dappTokenBalance: dappTokenBalance.toString( )})
+      // console.log({ balance: dappTokenBalance})  // 0 tokens 
+    } else {
+      window.alert('DappToken contract not deployed to detected network. ')
+    }
+
   }
 
   async loadWeb3() {
